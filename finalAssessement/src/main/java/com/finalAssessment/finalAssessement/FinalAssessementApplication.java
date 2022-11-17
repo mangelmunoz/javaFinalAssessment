80c1b956cc52485caa52e2253ebea534235eff7c
mangelmunoz <mangelmunoz877@gmail.com>
2022-11-17 09:32:21 +0100

    project created7

---
+++ finalAssessement/src/main/java/com/finalAssessment/finalAssessement/FinalAssessementApplication.java
@@ -0,0 +1,13 @@
+package com.finalAssessment.finalAssessement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssessementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAssessementApplication.class, args);
+	}
+
+}
---
+++ finalAssessement/src/test/java/com/finalAssessment/finalAssessement/FinalAssessementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalAssessment.finalAssessement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAssessementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
