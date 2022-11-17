b7f6aa9801d2e0e14d31233eafcbeb0383ecf406
mangelmunoz <mangelmunoz877@gmail.com>
2022-11-17 10:19:40 +0100

    schema.sql created

---
+++ finalAssessement/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE students(
+    id   INTEGER NULL AUTO_INCREMENT,
+    firstName VARCHAR(25) NULL,
+    lastName VARCHAR(25) NULL,
+    phoneNumber VARCHAR(2) NULL,
+    email VARCHAR(25) NULL,
+    PRIMARY KEY (id)
+);
+
